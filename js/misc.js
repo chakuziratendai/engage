886661568a8425ca1bc7acf10b14019c1b89de10
chakuziratendai <44413251+chakuziratendai@users.noreply.github.com>
2018-11-07 16:52:40 +0200

    Tracks Nav
    - added tracks navigation

--- js/misc.js
+++ js/misc.js
@@ -10,3 +10,27 @@ if ($(".navbar").offset().top > 50) {
     $('.navbar-brand img').attr('src','img/logo-white.png')
 }   
 });
+
+/* call owl carousel for tracks nav */
+$(document).ready(function() {
+             var owl = $('#tracks-nav');
+              owl.owlCarousel({
+                margin: 10,
+                nav: true,
+                loop: true,
+                dots: false,
+                navText : ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fas fa-angle-right"></i>'],
+                responsive: {
+                  0: {
+                    items: 1
+                  },
+                  600: {
+                    items: 3
+                  },
+                  1000: {
+                    items: 5
+                  }
+                }
+              })
+            })
+          
